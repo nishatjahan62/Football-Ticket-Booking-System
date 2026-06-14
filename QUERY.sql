19448c5192070e3bf6d522c63a332c5530805dd0
nishat jahan <nishatjahanposhpa@gmail.com>
2026-06-14 17:01:34 +0600

    feat and modify :included 3rd  query with is null , coalese  and modify 1st one with ::integer

--- QUERY.sql
+++ QUERY.sql
@@ -1,8 +1,20 @@
 -- Query :01 
-select match_id ,fixture , base_ticket_price 
+select match_id ,fixture , base_ticket_price::integer 
 from matches 
 where tournament_category = 'Champions League' and match_status = 'Available'
 
 -- Query 2: 
 select user_id, full_name , email  from users
 where full_name like 'Tanvir%' or full_name Ilike '%Haque%'
+
+-- Query 3: 
+select booking_id,user_id,match_id ,
+coalesce(payment_status,'Action Required') as systematic_status
+from bookings where payment_status is null
+
+-- Query 4: 
+
+-- Query 5:
+
+-- Query 6: 
+
